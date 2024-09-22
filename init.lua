1ec607f8fb9ccfe863aa00f2ff9b27f8d32b4bfc
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2024-09-23 00:34:40 +0530

    add treesitter, telescope & colorscheme

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -16,9 +17,33 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- ### PLUGINS ### --
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    {"rebelot/kanagawa.nvim"},
+    {
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
+       dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+require ('nvim-treesitter.install').compilers = { 'zig' }
+
+-- ### COLORSCHEME ### --
+vim.cmd("colorscheme kanagawa-dragon")
+
+-- ### TELESCOPE ### --
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- ### TREESITTER ### --
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+  ensure_installed = { "lua", "javascript", "typescript", "html", "css", "c" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
